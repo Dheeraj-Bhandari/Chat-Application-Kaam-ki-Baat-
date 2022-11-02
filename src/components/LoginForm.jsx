a7c1d30b926f82ae5e92b1fda538963c9ec5e106
Dheeraj kumar <Howtotech000@gmail.com>
2022-11-03 01:06:36 +0530

    chat Application (Kaam ki Baat)

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,42 +8,51 @@ const LoginForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-       const  authObject = {'Project-ID' : '17c557b0-9a68-4944-9b29-7b6cc4b5ac2f', 
-       'User-Name' : username ,
-        'User-Secret' : password}
+        const authObject = {
+            'Project-ID': '17c557b0-9a68-4944-9b29-7b6cc4b5ac2f',
+            'User-Name': username,
+            'User-Secret': password
+        }
 
-            try {
-              await  axios.get('https://api.chatengine.io/chats', {headers:authObject});
-              localStorage.setItem('chatengineusername', username); 
-              localStorage.setItem('chatenginepassword', password); 
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+            localStorage.setItem('chatengineusername', username);
+            localStorage.setItem('chatenginepassword', password);
 
-              window.location.reload();
+            window.location.reload();
 
-            } catch (error) {
-             seterror('Oops, incorrect Details')
-            }
+        } catch (error) {
+            seterror('Oops, incorrect Details')
+        }
     }
 
     return (
-        <div className='wrapper'>
+        <>
+            <div className='wrapper'>
 
-            <div className="form">
-                <h1 className="title">Kaam ki Baat</h1>
-                <h1 className="title" style={{fontSize: 'large'}}>Chat Application</h1>
-               
+                <div className="form">
+                    <h1 className="title">Kaam ki Baat</h1>
+                    <h1 className="title" style={{ fontSize: 'large' }}>Chat Application</h1>
 
-                <form action="" onSubmit={handleSubmit}>
-                    <input type="text" value={username} onChange={(e) => setusername(e.target.value)} className='input' placeholder='Username' required />
-                    <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} className='input' placeholder='Password' required />
-                    <div align='center'>
-                        <button type='submit' className='button'>
-                            <span>Start Chatting</span>
-                        </button>
-                    </div>
-                    <h2 className='error'>{error}</h2>
-                </form>
+
+                    <form action="" onSubmit={handleSubmit}>
+                        <input type="text" value={username} onChange={(e) => setusername(e.target.value)} className='input' placeholder='Username' required />
+                        <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} className='input' placeholder='Password' required />
+                        <div align='center'>
+                            <button type='submit' className='button'>
+                                <span>Start Chatting</span>
+                            </button>
+                        </div>
+                        <h2 className='error'>{error}</h2>
+                    </form>
+                </div>
+
             </div>
-        </div>
+            <div className='madewithlove' >
+                <p >Made With <span>❤</span> By <span><a href="https://www.linkedin.com/in/digitaldk/"
+                    target={'_blank'} rel="noreferrer" >Dheeraj Bhandadri</a></span></p>
+            </div>
+        </>
     )
 }
 
